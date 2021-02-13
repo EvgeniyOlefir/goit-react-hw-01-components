ad6bb0d5c781b43988cd0923d36b86fff013326c
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-02-13 10:23:09 +0200

    Создает проект React, старт ДЗ

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+React.createElement("a");
