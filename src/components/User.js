e1bbd83675d0bd9e54d52c1901f3cff9c0840356
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-02-13 13:29:15 +0200

    Добавляет PropTypes

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const User = ({ avatar, name, location, tag, statsFollowers, statsViews, statsLikes }) => (
   <div class="profile">
@@ -12,18 +13,34 @@ const User = ({ avatar, name, location, tag, statsFollowers, statsViews, statsLi
     <ul class="stats">
       <li>
         <span class="label">Followers</span>
-        <span class="quantity">{statsFollowers}</span>
+        <span class="quantity"> {statsFollowers}</span>
       </li>
       <li>
         <span class="label">Views</span>
-        <span class="quantity">{statsViews}</span>
+        <span class="quantity"> {statsViews}</span>
       </li>
       <li>
         <span class="label">Likes</span>
-        <span class="quantity">{statsLikes}</span>
+        <span class="quantity"> {statsLikes}</span>
       </li>
     </ul>
   </div>
 );
+
+User.defaultProps = {
+  statsFollowers: 0,
+  statsViews: 0,
+  statsLikes: 0,
+};
+
+User.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  statsFollowers: PropTypes.number,
+  statsViews: PropTypes.number,
+  statsLikes: PropTypes.number,
+};
 
 export default User;
