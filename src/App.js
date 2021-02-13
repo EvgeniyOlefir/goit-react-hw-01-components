2608fab14210775ce29b837a67b632b9a7937225
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-02-13 17:30:56 +0200

    Вторая задача не готова

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-
 import UserList from "./components/profile/UserList";
 import users from "./user.json";
+import Statistics from "./components/statistics/StatList";
+import statisticalData from "./statistical-data.json";
 
 const App = () => {
   return (
     <div>
       <UserList users={users} />
+      <Statistics title="Upload stats" stats={statisticalData} />;
+      <Statistics stats={statisticalData} />;
     </div>
   );
 };
