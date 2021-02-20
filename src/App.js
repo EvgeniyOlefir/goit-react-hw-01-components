8146fd86e5175e2cb9ac4e10b1ef4516ca20a71d
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-02-20 10:41:12 +0200

    Task 1 done without style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import UserList from "./components/profile/UserList";
-import users from "./user.json";
-import Statistics from "./components/statistics/StatList";
-import statisticalData from "./statistical-data.json";
+import Profile from "./components/Profile";
+import user from "./data/user.json";
 
 const App = () => {
   return (
     <div>
-      <UserList users={users} />
-      <Statistics title="Upload stats" stats={statisticalData} />;
-      <Statistics stats={statisticalData} />;
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Profile = ({ name, tag, location, avatar, stats }) => {
+  const { followers, views, likes } = stats;
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt="Аватар пользователя" class="avatar" />
+        <p class="name">{name}</p>
+        <p class="tag">@{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
