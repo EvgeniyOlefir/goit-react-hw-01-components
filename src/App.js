f14d637e0c2015ffde0fa0a19e1031c2662001d5
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-02-21 14:04:11 +0200

    Добавляет стилизацию задания 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
-import Profile from "./components/Profile";
-import Statistics from "./components/Statistics";
-import user from "./data/user.json";
-import statisticalData from "./data/statistical-data.json";
-import FriendList from "./components/FriendsList";
-import friends from "./data/friends.json";
+import React from 'react';
+import Profile from './components/Profile';
+import Statistics from './components/Statistics';
+import FriendList from './components/FriendsList';
+import TransactionHistory from './components/TransactionsHistory';
+
+import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 const App = () => {
   return (
@@ -19,6 +22,7 @@ const App = () => {
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
