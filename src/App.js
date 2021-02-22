450eb660102e359b9f86607ab62483a6f8c2902d
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-02-22 21:48:06 +0200

    Удалил задвоение статистики в задании 2

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={statisticalData} />
-      <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </div>
